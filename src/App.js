f74cb8047a7d283bafa94c9e5a945fe675a0746a
zeid <zeidhassan4@gmail.com>
2024-02-11 21:56:40 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     }
   };
 
-  const removeFromArmy = (botId) => {
+  function removeFromArmy (botId){
     const updatedArmy = army.filter((bot) => bot.id !== botId);
     setArmy(updatedArmy);
   };
